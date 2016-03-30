9acd1d5499754319361a3788528c9613d45ed771
Samael500 <samael500@gmail.com>
2016-03-30 15:51:53 +0300

    :repeat: upd settings py

--- settings.py
+++ settings.py
@@ -1,9 +1,25 @@
-settings = dict(
-    proj_ip = '10.1.1.123',
+VARS = dict(
+    # vagrant conf
+    proj_ip='10.1.1.123',
     box_name = 'debian/jessie64',
-
     vagrant_memory=2048,
     vagrant_cpus=2,
 
+    # include / declude config
     POSTGRES=True,
+    CELERY=False,
+    REDIS=False,
+
+    # project settings
+    proj_name=None,
+    root_dir=None,
+
+    # database config
+    db_name=None,
+    db_pass=None,
+    db_user=None,
+
+    # locale conf
+    locale='ru_RU',
+    encoding='UTF-8',
 )
