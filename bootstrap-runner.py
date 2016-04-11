9041c4ccf3875c72f4f203e74b68de72efb47a14
Maks <samael500@gmail.com>
2016-04-11 13:28:07 +0300

    :repeat: bootstarp project struct

---
+++ bootstrap-runner.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+""" Convenience wrapper for running falstart directly from source tree. """
+
+
+from falstart.falstart import main
+
+
+if __name__ == '__main__':
+    main()
